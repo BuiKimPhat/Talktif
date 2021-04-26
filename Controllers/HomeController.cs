80991b5340566081ee8ab93533b47b60d74da6f0
Chuong Thai <chuongthai885@gmail.com>
2021-04-26 08:38:13 +0700

    first time

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,9 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Talktif.Models;
+using Program.Models;
 
-namespace Talktif.Controllers
+namespace Program.Controllers
 {
     public class HomeController : Controller
     {
---
+++ Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Program.Models;
+
+namespace Program.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly ILogger<LoginController> _logger;
+
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
